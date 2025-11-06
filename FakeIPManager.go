0db636c14bb78c01fecc4411a7df372921c9f201
a1ex2k <aleksey7ananko@gmail.com>
2025-11-06 22:05:11 +0300

    fix

--- FakeIPManager.go
+++ FakeIPManager.go
@@ -4,11 +4,12 @@ import (
 	"encoding/binary"
 	"net"
 	"sync"
+	"sync/atomic"
 )
 
 const (
 	fakeIPv4Subnet     = uint32(0xC6120000) // 198.18.0.0
-	fakeIPMaxCount     = 131070             // Maximum number (~198.19.255.254)
+	fakeIPMaxCount     = 131070             // up to 198.19.255.254
 	fakeIPv6SubnetHigh = uint64(0xfddd_0000_0000_0000)
 )
 
@@ -19,6 +20,8 @@ type FakeIPManager struct {
 	rwMutex6            sync.RWMutex
 	addNftRule4Callback func(realIP net.IP, fakeIP net.IP) error
 	addNftRule6Callback func(realIP net.IP, fakeIP net.IP) error
+	nextIPv4Counter     atomic.Uint32
+	nextIPv6Counter     atomic.Uint64
 }
 
 func NewFakeIPManager(
@@ -26,6 +29,7 @@ func NewFakeIPManager(
 	nftCallback6 func(realIP, fakeIP net.IP) error) *FakeIPManager {
 	return &FakeIPManager{
 		ipv4Map:             make(map[IPv4Addr]net.IP),
+		ipv6Map:             make(map[IPv6Addr]net.IP),
 		addNftRule4Callback: nftCallback4,
 		addNftRule6Callback: nftCallback6,
 	}
@@ -35,24 +39,19 @@ func (m *FakeIPManager) GetFakeIP(domain string, realIP net.IP, ipVersion int) n
 	if realIP == nil {
 		return nil
 	}
-
-	var fakeIP net.IP
 	switch ipVersion {
 	case 4:
-		fakeIP = m.GetFakeIPv4(realIP)
+		return m.GetFakeIPv4(realIP)
 	case 6:
-		fakeIP = m.GetFakeIPv6(realIP)
-	}
-
-	if fakeIP == nil {
+		return m.GetFakeIPv6(realIP)
+	default:
 		return nil
 	}
-
-	return fakeIP
 }
 
 func (m *FakeIPManager) GetFakeIPv4(realIP net.IP) net.IP {
 	ip := MustIPv4FromIP(realIP)
+
 	m.rwMutex4.RLock()
 	fakeIP, ok := m.ipv4Map[ip]
 	m.rwMutex4.RUnlock()
@@ -62,23 +61,29 @@ func (m *FakeIPManager) GetFakeIPv4(realIP net.IP) net.IP {
 
 	m.rwMutex4.Lock()
 	defer m.rwMutex4.Unlock()
+
 	if fakeIP, ok := m.ipv4Map[ip]; ok {
 		return fakeIP
 	}
-	if len(m.ipv4Map) >= fakeIPMaxCount {
+
+	index := m.nextIPv4Counter.Add(1)
+	if index > fakeIPMaxCount {
+		return nil // out of addresses
+	}
+
+	fakeIP = IPv4FromUint32(fakeIPv4Subnet + index)
+	if err := m.addNftRule4Callback(realIP, fakeIP); err != nil {
+		m.nextIPv4Counter.Add(^uint32(0)) // decrement counter on failure
 		return nil
 	}
 
-	fakeIP = IPv4FromUint32(fakeIPv4Subnet + uint32(len(m.ipv4Map)+1))
-	if err := m.addNftRule4Callback(realIP, fakeIP); err != nil {
-		return nil
-	}
 	m.ipv4Map[ip] = fakeIP
 	return fakeIP
 }
 
 func (m *FakeIPManager) GetFakeIPv6(realIP net.IP) net.IP {
 	ip := MustIPv6FromIP(realIP)
+
 	m.rwMutex6.RLock()
 	fakeIP, ok := m.ipv6Map[ip]
 	m.rwMutex6.RUnlock()
@@ -88,22 +93,27 @@ func (m *FakeIPManager) GetFakeIPv6(realIP net.IP) net.IP {
 
 	m.rwMutex6.Lock()
 	defer m.rwMutex6.Unlock()
+
 	if fakeIP, ok := m.ipv6Map[ip]; ok {
 		return fakeIP
 	}
-	if len(m.ipv6Map) >= fakeIPMaxCount {
+
+	index := m.nextIPv6Counter.Add(1)
+	if index > fakeIPMaxCount {
 		return nil
 	}
 
-	fakeIP = IPv6FromUint64(fakeIPv6SubnetHigh, uint64(len(m.ipv6Map)+1))
+	fakeIP = IPv6FromUint64(fakeIPv6SubnetHigh, index)
 	if err := m.addNftRule6Callback(realIP, fakeIP); err != nil {
+		m.nextIPv6Counter.Add(^uint64(0)) // decrement on failure
 		return nil
 	}
+
 	m.ipv6Map[ip] = fakeIP
 	return fakeIP
 }
 
-func IPv6FromUint64(hi uint64, lo uint64) net.IP {
+func IPv6FromUint64(hi, lo uint64) net.IP {
 	var b [16]byte
 	binary.BigEndian.PutUint64(b[0:8], hi)
 	binary.BigEndian.PutUint64(b[8:16], lo)
